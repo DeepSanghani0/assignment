eda8c9e75087baf59b8f30ec17a8a28e1460e295
DeepSanghani0 <133763612+DeepSanghani0@users.noreply.github.com>
2023-05-24 15:42:04 +0530

    Add files via upload

---
+++ module1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+    printf("My Name is Sanghani Deep \n");
+    printf("Birth Date: 4th april 2000 \n");
+    printf("Age: 23 \n");
+    printf("Address: Nr. Hanuman temple, Theba, Jamnagar 361120 \n");
+
+}
